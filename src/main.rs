d007e2ee1665ad073851b792dfc217163e2e4954
Redfield-13 <fieldr62@gmail.com>
2024-09-07 11:00:27 +0200

    TODO_05 ADDED

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use chrono::Local;
 
 mod get_focus;
 mod detect_display_server;
+mod terminal_formatting;
 
 
 
@@ -19,7 +20,7 @@ fn main() {
     detect_display_server::detect_display_server();
     // HashMap to store the time spent on each application
     let mut app_times: HashMap<String, Duration> = HashMap::new();
-    let mut current_app = get_focus::get_focused_window().unwrap_or_else(|| "Unknown".to_string());
+    let mut current_window = get_focus::get_focused_window().unwrap_or_else(|| "Unknown".to_string());
     let mut start_time = Instant::now();
     
     println!("Starting application tracking...");
@@ -35,26 +36,30 @@ fn main() {
         let new_app: String = get_focus::get_focused_window().unwrap_or_else(|| "Unknown".to_string());
 
         // If the focused window has changed
-        if new_app != current_app {
+        if new_app != current_window {
             // Calculate the time spent on the previous window
             let elapsed = start_time.elapsed();
+            terminal_formatting::formatting();
             println!(
                 "{} -> Time spent on '{}': {:.2?} on this session: ",
                 Local::now().format("%Y-%m-%d %H:%M:%S"),
-                current_app,
+                current_window,
                 elapsed
             );
             
-
+            let splited: Vec<&str> = current_window.split("- ").collect();
+            let ser_split = serde_json::to_string(&splited).unwrap();
+            let current_app = String::from(splited[splited.len()-1]);
+            println!("{ser_split}");
+            
             // Add the time to the HashMap
-            *app_times.entry(current_app.clone()).or_insert(Duration::new(0, 0)) += elapsed;
-            println!("Total Time Spent on '{}': {:.2?}", current_app, app_times[&current_app]);
-            println!("\n");
-            println!("______________________________________________________");
-            println!("\n");
+            *app_times.entry(current_window.clone()).or_insert(Duration::new(0, 0)) += elapsed;
+            println!("Total Time Spent on '{}': {:.2?}", current_app, app_times[&current_window]);
+           
+            
 
             // Switch to the new window and reset the timer
-            current_app = new_app;
+            current_window = new_app;
             start_time = Instant::now();
         }
 
